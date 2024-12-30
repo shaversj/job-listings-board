57e5a933fde518362e59548fd71954c402aa111a
Josh Shavers <shaversj@gmail.com>
2024-12-30 08:16:31 -0600

    Add colors + fonts + images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,18 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "desaturated-dark-cyan": "hsl(180, 29%, 50%)",
+        "light-grayish-cyan-bg": "hsl(180, 52%, 96%)",
+        "light-grayish-cyan-filter": "hsl(180, 31%, 95%)",
+        "dark-grayish-cyan": "hsl(180, 8%, 52%)",
+        "very-dark-grayish-cyan": "hsl(180, 14%, 20%)",
+      },
+      fontFamily: {
+        spartan: ["var(--font-league-spartan)"],
       },
     },
   },
