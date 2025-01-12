23798ffaef2e8f274dd059d540e06936eb42375d
Josh Shavers <shaversj@gmail.com>
2025-01-12 11:54:37 -0600

    Add hover to badge

--- src/components/JobCategory.tsx
+++ src/components/JobCategory.tsx
@@ -8,7 +8,7 @@ interface JobCategoryProps {
 
 export default function JobCategory({ type, value, onClick }: JobCategoryProps) {
   return (
-    <button onClick={() => onClick(type, value)} className={"text-body-16px-bold inline rounded-s bg-light-grayish-cyan-filter px-[9px] pb-[3px] pt-[5px] text-desaturated-dark-cyan hover:bg-desaturated-dark-cyan hover:text-white"}>
+    <button onClick={() => onClick(type, value)} className={"text-body-16px-bold inline rounded-s-full bg-light-grayish-cyan-filter px-[9px] pb-[3px] pt-[5px] text-desaturated-dark-cyan hover:bg-desaturated-dark-cyan hover:text-white"}>
       {value}
     </button>
   );
